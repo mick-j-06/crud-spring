b5334f380706e57a1d5d37fc00f05c47a8c31f91
Micael Jerry <hei.jerry.2@gmail.com>
2022-08-06 16:14:24 +0300

    update controller (add post student)

--- src/main/java/com/example/crud/controller/SimpleController.java
+++ src/main/java/com/example/crud/controller/SimpleController.java
@@ -1,4 +1,12 @@
 package com.example.crud.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SimpleController {
+    @GetMapping(value = "/")
+    public String hello(){
+        return "✌️✌️✌️ HELLO ✌️✌️✌️";
+    }
 }
--- src/main/java/com/example/crud/controller/StudentController.java
+++ src/main/java/com/example/crud/controller/StudentController.java
@@ -1,4 +1,19 @@
 package com.example.crud.controller;
 
+import com.example.crud.model.Student;
+import com.example.crud.repository.StudentRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+    private StudentRepository studentRepository;
+
+    @PostMapping(value = "/student")
+    public Student createStudent(@RequestBody Student newStudent) {
+        studentRepository.save(newStudent);
+        return newStudent;
+    }
 }
