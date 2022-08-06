8e4db665fed9faa1b3c60a30fb6ee147bdc60b2f
Micael Jerry <hei.jerry.2@gmail.com>
2022-08-06 16:13:52 +0300

    application

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -1,4 +1,4 @@
-package com.example.helloworldapi;
+package com.example.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
